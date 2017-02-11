493e992f0725f9ecead6eb52e7d77b4325fd3997
Chase Hawthorne <ch73168@gmail.com>
2017-02-11 01:19:22 +0000

    Added msg

--- NetworkOverlay/src/MessagingNode.java
+++ NetworkOverlay/src/MessagingNode.java
@@ -1,13 +1,34 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class MessagingNode {
-	public static void main(String[] args){
-		Socket socket;
+	public static void main(String[] args) throws UnknownHostException, IOException{
+		/*Socket client = new Socket("localhost", 8000);
+		Scanner sc = new Scanner(System.in);
+
+		DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+		DataInputStream dis = new DataInputStream(client.getInputStream());*/
 		
-		try{
+		String test = "127.0.0.1";
+		
+		byte[] b = test.getBytes();
+		
+		for(byte temp: b){
+			System.out.println(temp + " " + (char) temp);
+		}
+		
+		/*while(true){
+			
+		}*/
+		
+		/*try{
 			socket = new Socket();
 		} catch(Exception e){
 			System.out.println("Exception trying to initialize Socket: " + e);
-		}
+		}*/
 	}
 }
--- NetworkOverlay/src/Registry.java
+++ NetworkOverlay/src/Registry.java
@@ -1,15 +1,39 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
 
 public class Registry {
 	final static int PORT = 8000;
+	static HashMap registry = new HashMap();
 	
-	public static void main(String[] args){
-		ServerSocket serverSocket;
+	public static void main(String[] args) throws IOException{
+		ServerSocket serverSocket = new ServerSocket(PORT);
 		
-		try{
+		while(true){
+			Socket client = serverSocket.accept();
+			System.out.println("Accepted.. " + client.getInetAddress() + " " + client.getPort());
+			
+			DataInputStream dis = new DataInputStream(client.getInputStream());
+			DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+			
+			readMessage(dis.readInt());
+		}
+		
+		/*try{
 			serverSocket = new ServerSocket(PORT);
 		} catch( Exception e){
 			System.out.print("Exception trying to initialize ServerSocket: " + e);
-		}
+		}*/
+	}
+	
+	public static void readMessage(int msgType){
+		System.out.println(msgType);
+	}
+	
+	public void register(){
+		
 	}
 }
